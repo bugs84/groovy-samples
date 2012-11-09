a4c768db39f05828f935300b09536450f18596c4
j vondrous <j.vondrous@vondrous038.cz.gmc.net>
2012-11-09 12:48:19 +0100

    Own Closure example

--- src/samples/Closures.groovy
+++ src/samples/Closures.groovy
@@ -11,3 +11,18 @@ assert 'abcde'.any { it > 'c' } //true if at least one member fulfill condition
 assert ['a', 'b', 'c'].grep {it >= 'b'} == ['b', 'c']
 assert 'abc'.collect { it * 2 } == ['aa', 'bb', 'cc']
 
+
+//OWN CLOSURE
+def closureResult = methodWithClosure("Param, ") {
+    it.append("Inside closure, ")
+}
+assert "Param, Before closure, Inside closure, After closure." == closureResult
+
+private String methodWithClosure(String param, Closure<StringBuilder> closure) {
+    def sb = new StringBuilder()
+    sb.append(param)
+    sb.append("Before closure, ")
+    closure.call(sb)
+    sb.append("After closure.")
+    sb.toString()
+}
